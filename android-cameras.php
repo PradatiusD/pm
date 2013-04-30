fab1a6dec57f88de41a52825cd845ba7f1b95bcd
PradatiusD <DanielPrada2012@gmail.com>
2013-04-29 23:30:00 -0400

    added win at facebook post

--- android-cameras.php
+++ android-cameras.php
@@ -1,92 +1,38 @@
-<table style="height: 125px;" width="100%" border="0" cellspacing="0" cellpadding="0">
-<tbody>
-<tr>
-<td style="font-family: Helvetica, Arial, sans-serif; color: #363636; line-height: 24px; font-size: 18px;">A new class of Android-connected cameras is just beginning to make its appearance (check out the <a href="http://picturemarketing.com/#/products/onsite/android-camera">Samsung Galaxy Camera</a>, for example). Running the FotoZap<sup>®</sup> app on one of these connected cameras turns it into the ultimate <a href="http://picturemarketing.com">social marketing tool</a>. Why? In the hands of your brand's front-line staff, FotoZap converts every face-to-face interaction into visual social media - a branded photo or video - that can be shared immediately on any social network.</td>
-</tr>
-<tr>
-<td style="height: 15px;"></td>
-</tr>
-<tr>
-<td><a href="http://picturemarketing.com/index.html#/products/onsite/android-camera"><img alt="Capture videos or photos and collect data with the FotoZap app for the Samsung Galaxy Camera and other Android cameras" src="http://picturemarketing.com/news/instant/androidcamera.jpg" border="0" /></a></td>
-</tr>
-<tr>
-<td style="height: 30px;"></td>
-</tr>
-<tr>
-<td><span style="color: #363636; font-family: Helvetica,Arial,sans-serif; font-size: 1.6em; font-weight: bold; line-height: 1.2em;">Hand your customers something that will spark sharing</span></td>
-</tr>
-<tr>
-<td style="height: 5px;"></td>
-</tr>
-<tr>
-<td style="font-family: Helvetica, Arial, sans-serif; color: #363636; line-height: 24px; font-size: 18px;">Hand out a branded photo print that displays a unique key code in the corner of the print. The code allows users to privately access a digital copy of their photo or video, directly through your brand's Facebook Page. The FotoZap system creates a "branded story" worth sharing, amplifies that sharing by giving consumers a reason to post on social networks, and collects data when users log on to see their images.The FotoZap app presents multiple ways to deliver the captured photo or video to a guest. Choose the method that best fits your particular promotion, or choose based on what is most convenient for each guest: hand out a branded print, type in an email address for instant photo delivery, or hand guests a branded card with retrieval instructions (and other messaging, incentives, or special offers).</td>
-</tr>
-<tr>
-<td style="font-family: Helvetica, Arial, sans-serif; color: #363636; line-height: 20px; font-size: 16px;"><span><a href="http://picturemarketing.com/index.html#/info/Heinz"><img alt="" src="http://picturemarketing.com/news/instant/printedphoto.jpg" border="0" /></a></span></td>
-</tr>
-<tr>
-<td style="vertical-align: text-top; font-style: italic; text-align: left;">Hand out a branded FotoZap® print to your event guest containing a private key to access their online photo.</td>
-</tr>
-<tr>
-<td style="height: 25px;"></td>
-</tr>
-<tr>
-<td style="font-family: Helvetica, Arial, sans-serif; color: #363636; line-height: 20px; font-size: 16px;"><span><a href="http://picturemarketing.com/index.html#/info/Heinz"><img alt="" src="http://picturemarketing.com/news/instant/cardhanding.jpg" border="0" /></a></span></td>
-</tr>
-<tr>
-<td style="/* [disabled]vertical-align: text-top; */ font-style: italic; text-align: left;">Or, hand your guest a pre-printed giveaway card containing a private key to their online photo. Giveaway cards make it easy for a brand to activate anywhere and at any scale.</td>
-</tr>
-<tr>
-<td style="height: 25px;"></td>
-</tr>
-<tr>
-<td style="font-family: Helvetica, Arial, sans-serif; color: #363636; line-height: 20px; font-size: 16px;"><span><a href="http://picturemarketing.com/index.html#/info/Heinz"><img alt="" src="http://picturemarketing.com/news/instant/giveawayoptions.jpg" border="0" /></a></span></td>
-</tr>
-<tr>
-<td style="vertical-align: text-top; font-style: italic; text-align: left;">Simply scan a card with the FotoZap app to activate it, or type in the guest's email address.</td>
-</tr>
-<tr>
-<td style="height: 35px;"></td>
-</tr>
-<tr>
-<td style="color: #363636; font-family: Helvetica,Arial,sans-serif; font-size: 1.6em; font-weight: bold; line-height: 1.2em;">Customers visit your Facebook Page to view their image and engage</td>
-</tr>
-<tr>
-<td style="height: 9px;"></td>
-</tr>
-<tr>
-<td style="font-family: Helvetica, Arial, sans-serif; color: #363636; line-height: 24px; font-size: 18px;">The link on the printed photo, branded giveaway card, or photo delivery email leads directly to your brand’s Facebook Page, where the consumer can view their branded image, share it, and take advantage of your coupon downloads, online promotions, or special offers. This is the perfect way to create true fans of your brand -- fans that actually engage with your content.<span><a href="http://picturemarketing.com/index.html#/info/Heinz"><img alt="" src="http://picturemarketing.com/news/instant/facebookphotopage.jpg" /></a></span></td>
-</tr>
-<tr>
-<td style="vertical-align: text-top; font-style: italic; text-align: left;">Let us build photo retrieval and sharing features right into your brand's <a href="http://picturemarketing.com/#/products/social/facebook-page-app">Facebook Page</a>.</td>
-</tr>
-<tr>
-<td style="height: 35px;"></td>
-</tr>
-<tr>
-<td style="color: #363636; font-family: Helvetica,Arial,sans-serif; font-size: 1.6em; font-weight: bold; line-height: 1.2em;">Convert your front-line staff into "super ambassadors" for your brand</td>
-</tr>
-<tr>
-<td style="height: 9px;"></td>
-</tr>
-<tr>
-<td style="font-family: Helvetica, Arial, sans-serif; color: #363636; line-height: 24px; font-size: 18px;"><a href="http://picturemarketing.com/#/process">Find out how</a> the FotoZap <a href="http://picturemarketing.com">social marketing toolset</a> turns every staff member into an ambassador for your brand. FotoZap is designed to be easy to learn, powerful to use, and offers the control and reporting that brands require.Running FotoZap on a connected camera does three things for your brand:
+A new class of Android-connected cameras is just beginning to make its appearance (check out the
+<a href="http://picturemarketing.com/#/products/onsite/android-camera">Samsung Galaxy Camera</a>, for example). Running the FotoZap<sup>®</sup> app on one of these connected cameras turns it into the ultimate<a href="http://picturemarketing.com">social marketing tool</a>. <!--more-->
+
+Why? In the hands of your brand's front-line staff, FotoZap converts every face-to-face interaction into visual social media - a branded photo or video - that can be shared immediately on any social network.
+
+<a href="http://picturemarketing.com/index.html#/products/onsite/android-camera"><img alt="Capture videos or photos and collect data with the FotoZap app for the Samsung Galaxy Camera and other Android cameras" src="http://picturemarketing.com/news/instant/androidcamera.jpg" border="0" /></a>
+<h5>Hand your customers something that will spark sharing.</h5>
+Hand out a branded photo print that displays a unique key code in the corner of the print. The code allows users to privately access a digital copy of their photo or video, directly through your brand's Facebook Page. The FotoZap system creates a "branded story" worth sharing, amplifies that sharing by giving consumers a reason to post on social networks, and collects data when users log on to see their images.The FotoZap app presents multiple ways to deliver the captured photo or video to a guest. Choose the method that best fits your particular promotion, or choose based on what is most convenient for each guest: hand out a branded print, type in an email address for instant photo delivery, or hand guests a branded card with retrieval instructions (and other messaging, incentives, or special offers).
+
+<a href="http://picturemarketing.com/index.html#/info/Heinz"><img alt="" src="http://picturemarketing.com/news/instant/printedphoto.jpg" border="0" /></a>
+
+Hand out a branded FotoZap® print to your event guest containing a private key to access their online photo.
+
+<a href="http://picturemarketing.com/index.html#/info/Heinz"><img alt="" src="http://picturemarketing.com/news/instant/cardhanding.jpg" border="0" /></a>
+
+Or, hand your guest a pre-printed giveaway card containing a private key to their online photo. Giveaway cards make it easy for a brand to activate anywhere and at any scale.
+
+<a href="http://picturemarketing.com/index.html#/info/Heinz"><img alt="" src="http://picturemarketing.com/news/instant/giveawayoptions.jpg" border="0" /></a>
+
+Simply scan a card with the FotoZap app to activate it, or type in the guest's email address.
+
+The link on the printed photo, branded giveaway card, or photo delivery email leads directly to your brand’s Facebook Page, where the consumer can view their branded image, share it, and take advantage of your coupon downloads, online promotions, or special offers. This is the perfect way to create true fans of your brand -- fans that actually engage with your content.
+
+<img alt="" src="http://picturemarketing.com/news/instant/facebookphotopage.jpg" />
+
+Let us build photo retrieval and sharing features right into your brand's <a href="http://picturemarketing.com/#/products/social/facebook-page-app">Facebook Page</a>.
+
+Convert your front-line staff into "super ambassadors" for your brand
+
+<a href="http://picturemarketing.com/#/process">Find out how</a> the FotoZap <a href="http://picturemarketing.com">social marketing toolset</a> turns every staff member into an ambassador for your brand. FotoZap is designed to be easy to learn, powerful to use, and offers the control and reporting that brands require.Running FotoZap on a connected camera does three things for your brand:
 <ol>
 	<li>It improves your staff's face-to-face interactions with consumers</li>
 	<li>It positions your brand at the center of photo sharing</li>
 	<li>It ensures that event and in-store experiences are amplified across social networks such as Facebook</li>
 </ol>
-</td>
-</tr>
-<tr>
-<td style="font-family: Helvetica, Arial, sans-serif; color: #363636; line-height: 20px; font-size: 16px;"><span><a href="http://www.picturemarketing.com/"><img alt="" src="http://picturemarketing.com/news/instant/brandambassador.jpg" border="0" /></a></span></td>
-</tr>
-<tr>
-<td style="height: 10px;"></td>
-</tr>
-<tr>
-<td style="font-family: Helvetica, Arial, sans-serif; color: #363636; line-height: 24px; font-size: 18px;"><span style="font-family: Helvetica, Arial, sans-serif; color: #363636; line-height: 24px; font-size: 18px;">Best of all, you can run FotoZap on any device. You probably have what you need right in your pocket or purse at this very moment. Android Cameras, iPads, iPhones, Android Smartphones, and our own proprietary FotoZap Cameras are all eagerly awaiting the moment when they can help turn your staff into social media superstars. Fulfill the dreams these devices are having! Download the FotoZap App right now, or <a href="http://picturemarketing.com/#/contactus">contact us</a></span>.</td>
-</tr>
-</tbody>
-</table>
-<!--- end of main table --->
+<a href="http://www.picturemarketing.com/"><img alt="" src="http://picturemarketing.com/news/instant/brandambassador.jpg" border="0" /></a>
+
+Best of all, you can run FotoZap on any device. You probably have what you need right in your pocket or purse at this very moment. Android Cameras, iPads, iPhones, Android Smartphones, and our own proprietary FotoZap Cameras are all eagerly awaiting the moment when they can help turn your staff into social media superstars. Fulfill the dreams these devices are having! Download the FotoZap App right now, or <a href="http://picturemarketing.com/#/contactus">contact us</a>.
