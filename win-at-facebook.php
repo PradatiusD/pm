43c9352638eda2ad46f3e07fae945118c327c483
PradatiusD <DanielPrada2012@gmail.com>
2013-04-29 23:38:36 -0400

    added scary social

--- win-at-facebook.php
+++ win-at-facebook.php
@@ -1,94 +1,26 @@
-<style type="text/css">
-a:link {
-     color: #0085ca;
-}
-a:hover {
-     color: #0077b3;
-}
-</style>
+<p>To win at Facebook you need both a fan base and content that your fans will want to share. This video describes how brands are building their audience by giving their customers what they want: personalized content that allows consumers to tell their own story. </p>
+<!--more-->
+<p>Take a look: <a href="http://www.picturemarketing.com/#/info/facebook">http://pct.re/Facebook</a></p>
+<p><a href="http://www.picturemarketing.com/#/info/facebook"><img border="0" src="http://info.picturemarketing.com/newsletters/facebook/video-facebook-win-609.jpg" width="609" alt=""></a></p>
 
-<table width="600px" border="0" cellspacing="0" cellpadding="0" style="margin:0 auto; text-align:left;">
-<tr>
-  <td valign="top" bgcolor="#FFFFFF">
-    <tr>
-      <td valign="top">
-           <table width="600" height="204" border="0" cellpadding="0" cellspacing="0">
+<h5>Where the rubber meets the road</h5>                                
+
+<p>The process is straightforward. Brand staff download FotoZap to a smartphone, tablet or connected camera. Then at events or in any face-to-face interaction, staff members capture photos and videos of customers enjoying the brand experience.  </p>
+<p>Customers visit your brand's Facebook page where they access their image, share it, and become a fan of your brand.  </p>
+<p>FotoZap enhances customer images by adding: branding, photo filters, and special offers.  </p>
 
 
-                  <tr>
-                    <td colspan="2" valign="top" style="width: 600px; height: 125px;">
+<p><a href="http://www.picturemarketing.com/#/info/facebook">
+  <img border="0" src="http://info.picturemarketing.com/newsletters/facebook/FotoZap-devices.jpg" width="609"  alt=""></a></p><br>
 
 
 
-                                               <!---start table--->
+<h5>Where the road meets the highway</h5>
+<p>Why do only 6% of consumers who become fans of a brand on Facebook take any additional action to engage with the brand? Most Facebook Brand Pages lack content worthy of engagement.</p>
 
+<p>FotoZap<sup>&reg;</sup> solves this problem with a unique social networking solution: <i>Photo Sharing for Brands</i>.</p>
 
-                                               
-                                                <table width="100%" border="0" cellspacing="0" cellpadding="0" style="height: 125px;">
-                           
-                                                  
-                                                  
+<p>FotoZap allows brands to use photo sharing in a way that consumers appreciate and staff members find easy to use. Brand managers can watch the increase in Facebook engagement as it happens.</p>
 
-                                                <tr>
-                                                  <td style="font-family: Helvetica, Arial, sans-serif; color: #363636; line-height:21px; font-size:18px;">
-                                                  
-                                <p>To win at Facebook you need both a fan base and content that your fans will want to share. This video describes how brands are building their audience by giving their customers what they want: personalized content that allows consumers to tell their own story. </p>
-                                <p>Take a look: <a href="http://www.picturemarketing.com/#/info/facebook">http://pct.re/Facebook</a></p>
-                                
-                                <p><a href="http://www.picturemarketing.com/#/info/facebook"><img border="0" src="http://info.picturemarketing.com/newsletters/facebook/video-facebook-win-609.jpg" width="609" alt=""></a></p>
-                                </td></tr>
-
-                                
-                                                  <tr>
-                                                    <td valign="top" bgcolor="#FFFFFF">
-                                                      <br /><br />
-                                                    <span style="color: #363636; display:block; padding:12px 0 0; font-family: Helvetica, Arial, sans-serif; font-size: 26px; font-weight: bold; line-height: 26px;"> Where the rubber meets the road</span><br /></td>
-                                                    </tr>
-                                
-                                                <tr>
-                                                  <td style="font-family: Helvetica, Arial, sans-serif; color: #363636; line-height:21px; font-size:18px;">
-                                        
-                                <p>The process is straightforward. Brand staff download FotoZap to a smartphone, tablet or connected camera. Then at events or in any face-to-face interaction, staff members capture photos and videos of customers enjoying the brand experience.  </p>
-                                <p>Customers visit your brand's Facebook page where they access their image, share it, and become a fan of your brand.  </p>
-                                <p>FotoZap enhances customer images by adding: branding, photo filters, and special offers.  </p>
-
-                                
-                                <p><a href="http://www.picturemarketing.com/#/info/facebook">
-                                <img border="0" src="http://info.picturemarketing.com/newsletters/facebook/FotoZap-devices.jpg" width="609"  alt=""></a></p><br>
-                                
-                                
-                                
-                                
-                                
-                                
-                                </td></tr>
-                                
-                                                  <tr>
-                                                    <td valign="top" bgcolor="#FFFFFF">
-                                
-                                                    <span style="color: #363636; display:block; padding:12px 0 0; font-family: Helvetica, Arial, sans-serif; font-size: 26px; font-weight: bold; line-height: 26px;"> Where the road meets the highway</span><br /></td>
-                                                    </tr>
-                                
-                                                <tr>
-                                                  <td style="font-family: Helvetica, Arial, sans-serif; color: #363636; line-height:21px; font-size:18px;">
-                                
-                                
-                                <p>Why do only 6% of consumers who become fans of a brand on Facebook take any additional action to engage with the brand? Most Facebook Brand Pages lack content worthy of  engagement.</p>
-                                
-                                
-                                <p>FotoZap<sup>&reg;</sup> solves this problem with a unique social networking solution: <i>Photo Sharing for Brands</i>.</p>
-                                
-
-                                
-                                <p>FotoZap allows brands to use photo sharing in a way that consumers appreciate and staff members find easy to use. Brand managers can watch the increase in Facebook engagement as it happens.</p>
-                                <p>Channel simple face-to-face interactions onto the superhighway of brand love with FotoZap. :)</p>
-                                                                <p> If you'd like to see how easy it is to drive true social engagement <a href="http://picturemarketing.com/#/contactus">contact us</a>.
-                                  </p>
-
-                                             </td>
-                                                </tr>
-                                        
-                                        </table>
-                                        <!---- end table --->
-                 </td></tr></table>
-                 </td></tr></table>
+<p>Channel simple face-to-face interactions onto the superhighway of brand love with FotoZap. :)</p>
+<p> If you'd like to see how easy it is to drive true social engagement <a href="http://picturemarketing.com/#/contactus">contact us</a>.</p>
